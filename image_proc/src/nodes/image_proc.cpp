a088996e27d01c9dba5b63f0b14d4786d81560c2
kurtkonolige <kurtkonolige@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-08-28 23:44:55 +0000

    image_proc working

--- image_proc/src/nodes/image_proc.cpp
+++ image_proc/src/nodes/image_proc.cpp
@@ -32,37 +32,110 @@
 *  POSSIBILITY OF SUCH DAMAGE.
 *********************************************************************/
 
-#include <opencv/cv.h>
-
 #include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <opencv_latest/CvBridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 
-#include <image_publisher/image_publisher.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/fill_image.h>
+#include <sensor_msgs/CameraInfo.h>
+
+#include <opencv_latest/CvBridge.h>
+#include <image_transport/image_publisher.h>
+
+#include "image.h"
+#include "cam_bridge.h"
 
 #include <boost/thread.hpp>
-#include <boost/format.hpp>
+
+using namespace std;
 
 //
 // This is the node creation file
-// Subscribes to a single image topic, and performs undistortion and 
+// Subscribes to a single image topic, and performs rectification and 
 //   color processing on the image
 //
 
 class ImageProc
 {
 private:
-  ros::NodeHandle node_handle_;
-  ros::V_Subscriber subs_;
+  ros::NodeHandle nh_;
+  message_filters::Subscriber<sensor_msgs::Image> image_sub_;
+  message_filters::Subscriber<sensor_msgs::CameraInfo> info_sub_;
+  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo> sync_;
   
-  sensor_msgs::Image msg2;
-  sensor_msgs::CvBridge img_bridge_;
-  
-  ImagePublisher img_pub_;
+  bool do_colorize_;
+  bool do_rectify_;
 
-  int count_;
+  image_transport::ImagePublisher pub_mono_;
+  image_transport::ImagePublisher pub_rect_;
+  image_transport::ImagePublisher pub_color_;
+  image_transport::ImagePublisher pub_rect_color_;
+
+  // @todo: maybe these should not be members?
+  cam::ImageData img_data_;
+  sensor_msgs::Image img_;
 
 public:
+
+  ImageProc(const ros::NodeHandle& nh) : nh_(nh), sync_(3)
+  {
+    std::string cam_name = nh_.resolveName("camera") + "/";
+    // we check camera node to see if we can do these
+    //    nh_.param(cam_name + "do_colorize", do_colorize_, false);
+    //    nh_.param(cam_name + "do_rectify", do_rectify_, false);
+
+    // Advertise outputs
+    pub_mono_.advertise(nh_, cam_name+"image_mono", 1);
+    pub_rect_.advertise(nh_, cam_name+"image_rect", 1);
+    pub_color_.advertise(nh_, cam_name+"image_color", 1);
+    pub_rect_color_.advertise(nh_, cam_name+"image_rect_color", 1);
+
+    // Subscribe to synchronized Image & CameraInfo topics
+    image_sub_.subscribe(nh_, cam_name + "image_raw", 1);
+    info_sub_.subscribe(nh_, cam_name + "camera_info", 1);
+    sync_.connectInput(image_sub_, info_sub_);
+    sync_.registerCallback(boost::bind(&ImageProc::imageCb, this, _1, _2));
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& raw_image, const sensor_msgs::CameraInfoConstPtr& cam_info)
+  {
+    cam_bridge::RawStereoToCamData(*raw_image, *cam_info, stereo_msgs::RawStereo::IMAGE_RAW, &img_data_);
+
+    // @todo: only do processing if topics have subscribers
+    // @todo: parameter for bayer interpolation to use
+    if (do_colorize_) {
+      //img_data_.colorConvertType = COLOR_CONVERSION_EDGE;
+      img_data_.doBayerColorRGB();
+      //img_data_.doBayerMono();
+    }
+
+    if (do_rectify_)
+      img_data_.doRectify();
+
+    // Publish images
+    img_.header.stamp = raw_image->header.stamp;
+    img_.header.frame_id = raw_image->header.frame_id;
+    publishImage(img_data_.imType, img_data_.im, img_data_.imSize, pub_mono_);
+    publishImage(img_data_.imColorType, img_data_.imColor, img_data_.imColorSize, pub_color_);
+    publishImage(img_data_.imRectType, img_data_.imRect, img_data_.imRectSize, pub_rect_);
+    publishImage(img_data_.imRectColorType, img_data_.imRectColor, img_data_.imRectColorSize, pub_rect_color_);
+  }
+
+  void publishImage(color_coding_t coding, void* data, size_t dataSize, const image_transport::ImagePublisher& pub)
+  {
+    if (coding == COLOR_CODING_NONE)
+      return;
+
+    // @todo: step calculation is a little hacky
+    fillImage(img_, cam_bridge::ColorCodingToImageEncoding(coding),
+              img_data_.imHeight, img_data_.imWidth,
+              dataSize / img_data_.imHeight /*step*/, data);
+    pub.publish(img_);
+  }
+
+
+#if 0
   ImageProc(const ros::NodeHandle& node_handle)
     : node_handle_(node_handle),       
       img_pub_(node_handle),
@@ -113,19 +186,20 @@ public:
       msg->encoding = "mono";
 #endif
   }
+#endif
 
 };
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "image_proc", ros::init_options::AnonymousName);
-  ros::NodeHandle n;
-  if (n.resolveName("image") == "/image") {
-    ROS_WARN("image_view: image has not been remapped! Example command-line usage:\n"
-             "\t$ rosrun image_view image_view image:=/forearm/image_color");
+  ros::NodeHandle nh;
+  if (nh.resolveName("camera") == "/camera") {
+    ROS_WARN("image_view: source image has not been remapped! Example command-line usage:\n"
+             "\t$ rosrun image_proc image_proc source:=/forearm-r");
   }
   
-  ImageProc proc(n);
+  ImageProc proc(nh);
 
   ros::spin();
   
