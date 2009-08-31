2061de29345fd406579f1907d015774a53dd4b39
gerkey <gerkey@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-08-31 19:10:05 +0000

    Renamed imaging_pipeline to image_pipeline

--- theora_image_transport/src/manifest.cpp
+++
@@ -1,7 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include "theora_image_transport/theora_publisher.h"
-#include "theora_image_transport/theora_subscriber.h"
-
-PLUGINLIB_REGISTER_CLASS(theora_pub, theora_image_transport::TheoraPublisher, image_transport::PublisherPlugin)
-
-PLUGINLIB_REGISTER_CLASS(theora_sub, theora_image_transport::TheoraSubscriber, image_transport::SubscriberPlugin)
