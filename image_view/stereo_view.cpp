068240710932c7667ebdd2b85cb15c9d6012f7a1
mihelich <mihelich@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-01-06 04:29:13 +0000

    image_view: Show disparity in stereo_view.

--- image_view/stereo_view.cpp
+++ image_view/stereo_view.cpp
@@ -37,22 +37,291 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <stereo_msgs/DisparityImage.h>
 #include <cv_bridge/CvBridge.h>
 
+#include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <image_transport/subscriber_filter.h>
 
 #include <boost/thread.hpp>
 #include <boost/format.hpp>
 
+namespace enc = sensor_msgs::image_encodings;
+
+// colormap for disparities, RGB
+static unsigned char colormap[768] = 
+  { 150, 150, 150,
+    107, 0, 12,
+    106, 0, 18,
+    105, 0, 24,
+    103, 0, 30,
+    102, 0, 36,
+    101, 0, 42,
+    99, 0, 48,
+    98, 0, 54,
+    97, 0, 60,
+    96, 0, 66,
+    94, 0, 72,
+    93, 0, 78,
+    92, 0, 84,
+    91, 0, 90,
+    89, 0, 96,
+    88, 0, 102,
+    87, 0, 108,
+    85, 0, 114,
+    84, 0, 120,
+    83, 0, 126,
+    82, 0, 131,
+    80, 0, 137,
+    79, 0, 143,
+    78, 0, 149,
+    77, 0, 155,
+    75, 0, 161,
+    74, 0, 167,
+    73, 0, 173,
+    71, 0, 179,
+    70, 0, 185,
+    69, 0, 191,
+    68, 0, 197,
+    66, 0, 203,
+    65, 0, 209,
+    64, 0, 215,
+    62, 0, 221,
+    61, 0, 227,
+    60, 0, 233,
+    59, 0, 239,
+    57, 0, 245,
+    56, 0, 251,
+    55, 0, 255,
+    54, 0, 255,
+    52, 0, 255,
+    51, 0, 255,
+    50, 0, 255,
+    48, 0, 255,
+    47, 0, 255,
+    46, 0, 255,
+    45, 0, 255,
+    43, 0, 255,
+    42, 0, 255,
+    41, 0, 255,
+    40, 0, 255,
+    38, 0, 255,
+    37, 0, 255,
+    36, 0, 255,
+    34, 0, 255,
+    33, 0, 255,
+    32, 0, 255,
+    31, 0, 255,
+    29, 0, 255,
+    28, 0, 255,
+    27, 0, 255,
+    26, 0, 255,
+    24, 0, 255,
+    23, 0, 255,
+    22, 0, 255,
+    20, 0, 255,
+    19, 0, 255,
+    18, 0, 255,
+    17, 0, 255,
+    15, 0, 255,
+    14, 0, 255,
+    13, 0, 255,
+    11, 0, 255,
+    10, 0, 255,
+    9, 0, 255,
+    8, 0, 255,
+    6, 0, 255,
+    5, 0, 255,
+    4, 0, 255,
+    3, 0, 255,
+    1, 0, 255,
+    0, 4, 255,
+    0, 10, 255,
+    0, 16, 255,
+    0, 22, 255,
+    0, 28, 255,
+    0, 34, 255,
+    0, 40, 255,
+    0, 46, 255,
+    0, 52, 255,
+    0, 58, 255,
+    0, 64, 255,
+    0, 70, 255,
+    0, 76, 255,
+    0, 82, 255,
+    0, 88, 255,
+    0, 94, 255,
+    0, 100, 255,
+    0, 106, 255,
+    0, 112, 255,
+    0, 118, 255,
+    0, 124, 255,
+    0, 129, 255,
+    0, 135, 255,
+    0, 141, 255,
+    0, 147, 255,
+    0, 153, 255,
+    0, 159, 255,
+    0, 165, 255,
+    0, 171, 255,
+    0, 177, 255,
+    0, 183, 255,
+    0, 189, 255,
+    0, 195, 255,
+    0, 201, 255,
+    0, 207, 255,
+    0, 213, 255,
+    0, 219, 255,
+    0, 225, 255,
+    0, 231, 255,
+    0, 237, 255,
+    0, 243, 255,
+    0, 249, 255,
+    0, 255, 255,
+    0, 255, 249,
+    0, 255, 243,
+    0, 255, 237,
+    0, 255, 231,
+    0, 255, 225,
+    0, 255, 219,
+    0, 255, 213,
+    0, 255, 207,
+    0, 255, 201,
+    0, 255, 195,
+    0, 255, 189,
+    0, 255, 183,
+    0, 255, 177,
+    0, 255, 171,
+    0, 255, 165,
+    0, 255, 159,
+    0, 255, 153,
+    0, 255, 147,
+    0, 255, 141,
+    0, 255, 135,
+    0, 255, 129,
+    0, 255, 124,
+    0, 255, 118,
+    0, 255, 112,
+    0, 255, 106,
+    0, 255, 100,
+    0, 255, 94,
+    0, 255, 88,
+    0, 255, 82,
+    0, 255, 76,
+    0, 255, 70,
+    0, 255, 64,
+    0, 255, 58,
+    0, 255, 52,
+    0, 255, 46,
+    0, 255, 40,
+    0, 255, 34,
+    0, 255, 28,
+    0, 255, 22,
+    0, 255, 16,
+    0, 255, 10,
+    0, 255, 4,
+    2, 255, 0,
+    8, 255, 0,
+    14, 255, 0,
+    20, 255, 0,
+    26, 255, 0,
+    32, 255, 0,
+    38, 255, 0,
+    44, 255, 0,
+    50, 255, 0,
+    56, 255, 0,
+    62, 255, 0,
+    68, 255, 0,
+    74, 255, 0,
+    80, 255, 0,
+    86, 255, 0,
+    92, 255, 0,
+    98, 255, 0,
+    104, 255, 0,
+    110, 255, 0,
+    116, 255, 0,
+    122, 255, 0,
+    128, 255, 0,
+    133, 255, 0,
+    139, 255, 0,
+    145, 255, 0,
+    151, 255, 0,
+    157, 255, 0,
+    163, 255, 0,
+    169, 255, 0,
+    175, 255, 0,
+    181, 255, 0,
+    187, 255, 0,
+    193, 255, 0,
+    199, 255, 0,
+    205, 255, 0,
+    211, 255, 0,
+    217, 255, 0,
+    223, 255, 0,
+    229, 255, 0,
+    235, 255, 0,
+    241, 255, 0,
+    247, 255, 0,
+    253, 255, 0,
+    255, 251, 0,
+    255, 245, 0,
+    255, 239, 0,
+    255, 233, 0,
+    255, 227, 0,
+    255, 221, 0,
+    255, 215, 0,
+    255, 209, 0,
+    255, 203, 0,
+    255, 197, 0,
+    255, 191, 0,
+    255, 185, 0,
+    255, 179, 0,
+    255, 173, 0,
+    255, 167, 0,
+    255, 161, 0,
+    255, 155, 0,
+    255, 149, 0,
+    255, 143, 0,
+    255, 137, 0,
+    255, 131, 0,
+    255, 126, 0,
+    255, 120, 0,
+    255, 114, 0,
+    255, 108, 0,
+    255, 102, 0,
+    255, 96, 0,
+    255, 90, 0,
+    255, 84, 0,
+    255, 78, 0,
+    255, 72, 0,
+    255, 66, 0,
+    255, 60, 0,
+    255, 54, 0,
+    255, 48, 0,
+    255, 42, 0,
+    255, 36, 0,
+    255, 30, 0,
+    255, 24, 0,
+    255, 18, 0,
+    255, 12, 0,
+    255,  6, 0,
+    255,  0, 0,
+  };
+
 class StereoView
 {
 private:
+  image_transport::ImageTransport it_;
   image_transport::SubscriberFilter left_sub_, right_sub_;
-  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync_;
+  message_filters::Subscriber<stereo_msgs::DisparityImage> disparity_sub_;
+  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image,
+                                    stereo_msgs::DisparityImage> sync_;
   
   sensor_msgs::ImageConstPtr last_left_, last_right_;
   sensor_msgs::CvBridge left_bridge_, right_bridge_;
+  cv::Mat_<cv::Vec3b> disparity_color_;
   boost::mutex image_mutex_;
   
   boost::format filename_format_;
@@ -60,7 +329,7 @@ private:
 
 public:
   StereoView(ros::NodeHandle& nh)
-    : sync_(3), filename_format_(""), count_(0)
+    : it_(nh), sync_(3), filename_format_(""), count_(0)
   {
     ros::NodeHandle local_nh("~");
     bool autosize;
@@ -72,16 +341,17 @@ public:
     
     cvNamedWindow("left", autosize ? CV_WINDOW_AUTOSIZE : 0);
     cvNamedWindow("right", autosize ? CV_WINDOW_AUTOSIZE : 0);
+    cv::namedWindow("disparity", autosize ? CV_WINDOW_AUTOSIZE : 0);
     cvSetMouseCallback("left",  &StereoView::mouseCB, this);
     cvSetMouseCallback("right", &StereoView::mouseCB, this);
     cvStartWindowThread();
 
     std::string left_topic = nh.resolveName("stereo") + "/left/" + nh.resolveName("image");
     std::string right_topic = nh.resolveName("stereo") + "/right/" + nh.resolveName("image");
-    left_sub_.subscribe(nh, left_topic, 3);
-    right_sub_.subscribe(nh, right_topic, 3);
+    left_sub_.subscribe(it_, left_topic, 3);
+    right_sub_.subscribe(it_, right_topic, 3);
     sync_.connectInput(left_sub_, right_sub_);
-    sync_.registerCallback(boost::bind(&StereoView::imageCB, this, _1, _2));
+    sync_.registerCallback(boost::bind(&StereoView::imageCB, this, _1, _2, _3));
   }
 
   ~StereoView()
@@ -90,23 +360,23 @@ public:
     cvDestroyWindow("right");
   }
 
-  void showImage(const char* window, const sensor_msgs::ImageConstPtr& img,
+  void showImage(const char* window, const sensor_msgs::Image& img,
                  sensor_msgs::CvBridge& bridge)
   {
     // May want to view raw bayer data
-    if (img->encoding.find("bayer") != std::string::npos)
-      boost::const_pointer_cast<sensor_msgs::Image>(img)->encoding = "mono8";
+    if (img.encoding.find("bayer") != std::string::npos)
+      const_cast<sensor_msgs::Image&>(img).encoding = enc::MONO8;
 
-    if (boost::const_pointer_cast<sensor_msgs::Image>(img)->encoding == "mono8" &&
-        bridge.fromImage(*img, "mono8"))
+    if (img.encoding == enc::MONO8 && bridge.fromImage(img, enc::MONO8))
       cvShowImage(window, bridge.toIpl());
-    else if (bridge.fromImage(*img, "bgr8"))
+    else if (bridge.fromImage(img, enc::BGR8))
       cvShowImage(window, bridge.toIpl());
     else
-      ROS_ERROR("Unable to convert %s image to bgr8", img->encoding.c_str());
+      ROS_ERROR("Unable to convert %s image to bgr8", img.encoding.c_str());
   }
 
-  void imageCB(const sensor_msgs::ImageConstPtr& left, const sensor_msgs::ImageConstPtr& right)
+  void imageCB(const sensor_msgs::ImageConstPtr& left, const sensor_msgs::ImageConstPtr& right,
+               const stereo_msgs::DisparityImageConstPtr& disparity)
   {
     {
       boost::lock_guard<boost::mutex> guard(image_mutex_);
@@ -116,8 +386,31 @@ public:
       last_right_ = right;
     }
 
-    showImage("left", left, left_bridge_);
-    showImage("right", right, right_bridge_);
+    showImage("left", *left, left_bridge_);
+    showImage("right", *right, right_bridge_);
+
+    // Colormap and display the disparity image
+    double f = disparity->f;
+    double T = disparity->T;
+    double min_disparity = f*T / disparity->max_depth;
+    double max_disparity = f*T / disparity->min_depth;
+    double inv_disparity_range = 1.0 / (max_disparity - min_disparity);
+
+    assert(disparity->image.encoding == enc::TYPE_32FC1);
+    disparity_color_.create(disparity->image.height, disparity->image.width);
+    for (int row = 0; row < disparity_color_.rows; ++row) {
+      const float* d = (const float*)(&disparity->image.data[row*disparity->image.step]);
+      for (int col = 0; col < disparity_color_.cols; ++col) {
+        int index = (d[col] - min_disparity) * inv_disparity_range + 0.5;
+        index = std::min(255, std::max(0, index));
+        // Fill as BGR
+        disparity_color_(row, col)[2] = colormap[3*index + 0];
+        disparity_color_(row, col)[1] = colormap[3*index + 1];
+        disparity_color_(row, col)[0] = colormap[3*index + 2];
+      }
+    }
+
+    cv::imshow("disparity", disparity_color_);
   }
 
   void saveImage(const char* prefix, const IplImage* image)
