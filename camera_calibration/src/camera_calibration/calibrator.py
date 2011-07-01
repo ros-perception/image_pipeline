26362409d2240a40e02846bb250abf314a07616f
mihelich <mihelich@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2011-07-01 05:03:13 +0000

    camera_calibration: Use fast check option to FindChessboardCorners to avoid big slowdown when no checkerboard is visible.

--- camera_calibration/src/camera_calibration/calibrator.py
+++ camera_calibration/src/camera_calibration/calibrator.py
@@ -492,12 +492,17 @@ class MonoCalibrator(Calibrator):
         original image are in calibrated image).
         """
 
-        ncm = cv.CreateMat(3, 3, cv.CV_64FC1)
-        cv.GetOptimalNewCameraMatrix(self.intrinsics, self.distortion, self.size, a, ncm)
-        cv.InitUndistortRectifyMap(self.intrinsics, self.distortion, self.R, ncm, self.mapx, self.mapy)
+        if 0:
+            ncm = cv.CreateMat(3, 3, cv.CV_64FC1)
+            cv.GetOptimalNewCameraMatrix(self.intrinsics, self.distortion, self.size, a, ncm)
+            cv.InitUndistortRectifyMap(self.intrinsics, self.distortion, self.R, ncm, self.mapx, self.mapy)
 
-        cv.SetZero(self.P)
-        cv.Copy(ncm, cv.GetSubRect(self.P, (0, 0, 3, 3)))
+            cv.SetZero(self.P)
+            cv.Copy(ncm, cv.GetSubRect(self.P, (0, 0, 3, 3)))
+        else:
+            cv.InitUndistortRectifyMap(self.intrinsics, self.distortion, self.R, self.intrinsics, self.mapx, self.mapy)
+            cv.SetZero(self.P)
+            cv.Copy(self.intrinsics, cv.GetSubRect(self.P, (0, 0, 3, 3)))
 
     def remap(self, src):
         """
