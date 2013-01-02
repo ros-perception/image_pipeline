3730e114b689a3a07ba00dd57cba6d52a5559615
Vincent Rabaud <vrabaud@willowgarage.com>
2013-01-03 00:04:32 +0100

    fix the bad Python integration

--- camera_calibration/setup.py
+++ camera_calibration/setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
-from catkin_pkg.package import parse_package_for_distutils
+from catkin_pkg.python_setup import generate_distutils_setup
 
-d = parse_package_for_distutils()
+d = generate_distutils_setup()
 d['packages'] = ['camera_calibration']
 d['package_dir'] = {'':'src'}
-)
 
 setup(**d)
