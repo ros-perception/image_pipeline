5630a524d861927a477f71137d7e45b61ac1abef
Vincent Rabaud <vincent.rabaud@gmail.com>
2014-03-02 12:44:59 +0100

    fix the multiboard calibration test

--- camera_calibration/test/multiple_boards.py
+++ camera_calibration/test/multiple_boards.py
@@ -60,7 +60,7 @@ class TestMultipleBoards(unittest.TestCase):
 
         stereo_cal = StereoCalibrator([board, small_board])
         
-        my_archive_name = "multi_board_calibration.tar.gz"
+        my_archive_name = roslib.packages.find_resource('camera_calibration', 'multi_board_calibration.tar.gz')[0]
         stereo_cal.do_tarfile_calibration(my_archive_name)
 
         stereo_cal.report()
