7f7f3e3f04b7a08800497bef36f7f3e0d47b152a
Vincent Rabaud <vincent.rabaud@gmail.com>
2014-08-19 06:49:48 +0200

    install scripts in the local bin (they are now rosrun-able again) fixes #93

--- camera_calibration/setup.py
+++ camera_calibration/setup.py
@@ -5,9 +5,5 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup()
 d['packages'] = ['camera_calibration']
 d['package_dir'] = {'':'src'}
-d['scripts'] = ['nodes/cameracalibrator.py',
-                'nodes/cameracheck.py',
-                'nodes/camera_hammer.py',
-                'scripts/tarfile_calibration.py']
 
 setup(**d)
