ea6464a5ef705c66539b5d361a887e0d60ba40ae
Chris Ye <chris.ye@intel.com>
2018-11-22 06:03:09 +0800

    Port depth image proc on ROS2 (#362)
    * Port depth_image_proc on ROS2
    
    * Port depth_image_proc of image_pipeline on ROS2
    
    * rename Nodelets as Node
    
    * add launch examples, such as "ros2 launch depth_image_proc point_cloud_xyzi.launch.py"
    
    * verified point_cloud_xyzrgb, point_cloud_xyz, convert_metric based on Realsense camera(https://github.com/intel/ros2_intel_realsense).
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * add ament_lint_auto test and adjust code style
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * update test example for depth_image_proc
    
    - rename raw topic as image_transport fixed the issue (https://github.com/ros-perception/image_common/issues/96)
    - update test example of point_cloud_xyzrgb.launch
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * remove unused dependence in cmakelist
    
    * remove boost which is unused on ROS2
    
    * remove cv_bridge version check logic as setted in find_package
    
    * update maintainer in package.xml
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * added all example launchers for demo test
    
    pass test:
     ros2 launch depth_image_proc point_cloud_xyzrgb.launch.py
     ros2 launch depth_image_proc point_cloud_xyz.launch.py
     ros2 launch depth_image_proc convert_metric.launch.py
     ros2 launch depth_image_proc crop_foremost.launch.py
     ros2 launch depth_image_proc point_cloud_xyz_radial.launch.py
     ros2 launch depth_image_proc disparity.launch.py
     ros2 launch depth_image_proc register.launch.py
     ros2 launch depth_image_proc point_cloud_xyzi.launch.py
     ros2 launch depth_image_proc point_cloud_xyzi_radial.launch.py
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * @wip update to use raw pointers.
    
    * continue to update to use raw pointer
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>

--- depth_image_proc/src/nodelets/convert_metric.cpp
+++ depth_image_proc/src/nodelets/convert_metric.cpp
@@ -1,140 +1,142 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
-#include <ros/ros.h>
-#include <nodelet/nodelet.h>
+// Copyright (c) 2008, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the Willow Garage nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <rclcpp/rclcpp.hpp>
 #include <image_transport/image_transport.h>
-#include <sensor_msgs/image_encodings.h>
-#include <boost/thread.hpp>
+#include <sensor_msgs/image_encodings.hpp>
+#include <depth_image_proc/visibility.h>
+#include <cmath>
+#include <memory>
+#include <limits>
 
-namespace depth_image_proc {
+namespace depth_image_proc
+{
 
 namespace enc = sensor_msgs::image_encodings;
 
-class ConvertMetricNodelet : public nodelet::Nodelet
+class ConvertMetricNode : public rclcpp::Node
 {
+public:
+  DEPTH_IMAGE_PROC_PUBLIC ConvertMetricNode();
+
+private:
   // Subscriptions
-  boost::shared_ptr<image_transport::ImageTransport> it_;
   image_transport::Subscriber sub_raw_;
 
   // Publications
-  boost::mutex connect_mutex_;
+  std::mutex connect_mutex_;
   image_transport::Publisher pub_depth_;
-
-  virtual void onInit();
 
   void connectCb();
 
-  void depthCb(const sensor_msgs::ImageConstPtr& raw_msg);
+  void depthCb(const sensor_msgs::msg::Image::ConstSharedPtr & raw_msg);
+
+  rclcpp::Logger logger_ = rclcpp::get_logger("ConvertMetricNode");
 };
 
-void ConvertMetricNodelet::onInit()
+ConvertMetricNode::ConvertMetricNode()
+: Node("ConvertMetricNode")
 {
-  ros::NodeHandle& nh = getNodeHandle();
-  it_.reset(new image_transport::ImageTransport(nh));
-
   // Monitor whether anyone is subscribed to the output
-  image_transport::SubscriberStatusCallback connect_cb = boost::bind(&ConvertMetricNodelet::connectCb, this);
+  // TODO(ros2) Implement when SubscriberStatusCallback is available
+  // image_transport::SubscriberStatusCallback connect_cb =
+  //     std::bind(&ConvertMetricNode::connectCb, this);
+  connectCb();
+
   // Make sure we don't enter connectCb() between advertising and assigning to pub_depth_
-  boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  pub_depth_ = it_->advertise("image", 1, connect_cb, connect_cb);
+  std::lock_guard<std::mutex> lock(connect_mutex_);
+  // TODO(ros2) Implement when SubscriberStatusCallback is available
+  // pub_depth_ = it_->advertise("image", 1, connect_cb, connect_cb);
+  pub_depth_ = image_transport::create_publisher(this, "image");
 }
 
 // Handles (un)subscribing when clients (un)subscribe
-void ConvertMetricNodelet::connectCb()
+void ConvertMetricNode::connectCb()
 {
-  boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  if (pub_depth_.getNumSubscribers() == 0)
-  {
+  std::lock_guard<std::mutex> lock(connect_mutex_);
+  // TODO(ros2) Implement getNumSubscribers when rcl/rmw support it
+  // if (pub_depth_.getNumSubscribers() == 0)
+  if (0) {
     sub_raw_.shutdown();
-  }
-  else if (!sub_raw_)
-  {
-    image_transport::TransportHints hints("raw", ros::TransportHints(), getPrivateNodeHandle());
-    sub_raw_ = it_->subscribe("image_raw", 1, &ConvertMetricNodelet::depthCb, this, hints);
+  } else if (!sub_raw_) {
+    image_transport::TransportHints hints(this, "raw");
+    sub_raw_ = image_transport::create_subscription(this, "image_raw",
+        std::bind(&ConvertMetricNode::depthCb, this, std::placeholders::_1),
+        hints.getTransport());
   }
 }
 
-void ConvertMetricNodelet::depthCb(const sensor_msgs::ImageConstPtr& raw_msg)
+void ConvertMetricNode::depthCb(const sensor_msgs::msg::Image::ConstSharedPtr & raw_msg)
 {
-  // Allocate new Image message
-  sensor_msgs::ImagePtr depth_msg( new sensor_msgs::Image );
-  depth_msg->header   = raw_msg->header;
-  depth_msg->height   = raw_msg->height;
-  depth_msg->width    = raw_msg->width;
+  auto depth_msg = std::make_shared<sensor_msgs::msg::Image>();
+  depth_msg->header = raw_msg->header;
+  depth_msg->height = raw_msg->height;
+  depth_msg->width = raw_msg->width;
 
   // Set data, encoding and step after converting the metric.
-  if (raw_msg->encoding == enc::TYPE_16UC1)
-  {
+  if (raw_msg->encoding == enc::TYPE_16UC1) {
     depth_msg->encoding = enc::TYPE_32FC1;
-    depth_msg->step     = raw_msg->width * (enc::bitDepth(depth_msg->encoding) / 8);
+    depth_msg->step = raw_msg->width * (enc::bitDepth(depth_msg->encoding) / 8);
     depth_msg->data.resize(depth_msg->height * depth_msg->step);
     // Fill in the depth image data, converting mm to m
-    float bad_point = std::numeric_limits<float>::quiet_NaN ();
-    const uint16_t* raw_data = reinterpret_cast<const uint16_t*>(&raw_msg->data[0]);
-    float* depth_data = reinterpret_cast<float*>(&depth_msg->data[0]);
-    for (unsigned index = 0; index < depth_msg->height * depth_msg->width; ++index)
-    {
+    float bad_point = std::numeric_limits<float>::quiet_NaN();
+    const uint16_t * raw_data = reinterpret_cast<const uint16_t *>(&raw_msg->data[0]);
+    float * depth_data = reinterpret_cast<float *>(&depth_msg->data[0]);
+    for (unsigned index = 0; index < depth_msg->height * depth_msg->width; ++index) {
       uint16_t raw = raw_data[index];
-      depth_data[index] = (raw == 0) ? bad_point : (float)raw * 0.001f;
+      depth_data[index] = (raw == 0) ? bad_point : static_cast<float>(raw * 0.001f);
     }
-  }
-  else if (raw_msg->encoding == enc::TYPE_32FC1)
-  {
+  } else if (raw_msg->encoding == enc::TYPE_32FC1) {
     depth_msg->encoding = enc::TYPE_16UC1;
-    depth_msg->step     = raw_msg->width * (enc::bitDepth(depth_msg->encoding) / 8);
+    depth_msg->step = raw_msg->width * (enc::bitDepth(depth_msg->encoding) / 8);
     depth_msg->data.resize(depth_msg->height * depth_msg->step);
     // Fill in the depth image data, converting m to mm
     uint16_t bad_point = 0;
-    const float* raw_data = reinterpret_cast<const float*>(&raw_msg->data[0]);
-    uint16_t* depth_data = reinterpret_cast<uint16_t*>(&depth_msg->data[0]);
-    for (unsigned index = 0; index < depth_msg->height * depth_msg->width; ++index)
-    {
+    const float * raw_data = reinterpret_cast<const float *>(&raw_msg->data[0]);
+    uint16_t * depth_data = reinterpret_cast<uint16_t *>(&depth_msg->data[0]);
+    for (unsigned index = 0; index < depth_msg->height * depth_msg->width; ++index) {
       float raw = raw_data[index];
-      depth_data[index] = std::isnan(raw) ? bad_point : (uint16_t)(raw * 1000);
+      depth_data[index] = std::isnan(raw) ? bad_point : static_cast<uint16_t>(raw * 1000);
     }
-  }
-  else
-  {
-    ROS_ERROR("Unsupported image conversion from %s.", raw_msg->encoding.c_str());
+  } else {
+    RCLCPP_ERROR(logger_, "Unsupported image conversion from %s.", raw_msg->encoding.c_str());
     return;
   }
 
   pub_depth_.publish(depth_msg);
 }
 
-} // namespace depth_image_proc
+}  // namespace depth_image_proc
 
-// Register as nodelet
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(depth_image_proc::ConvertMetricNodelet,nodelet::Nodelet);
+#include "class_loader/register_macro.hpp"
+
+// Register the component with class_loader.
+CLASS_LOADER_REGISTER_CLASS(depth_image_proc::ConvertMetricNode, rclcpp::Node)
--- depth_image_proc/src/nodelets/crop_foremost.cpp
+++ depth_image_proc/src/nodelets/crop_foremost.cpp
@@ -1,111 +1,115 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
-//#include <ros/ros.h>
-#include <nodelet/nodelet.h>
+// Copyright (c) 2008, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the Willow Garage nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <rclcpp/rclcpp.hpp>
 #include <image_transport/image_transport.h>
-#include <boost/thread.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <depth_image_proc/visibility.h>
+#include <memory>
 
-namespace depth_image_proc {
+namespace depth_image_proc
+{
 
 namespace enc = sensor_msgs::image_encodings;
 
-class CropForemostNodelet : public nodelet::Nodelet
+class CropForemostNode : public rclcpp::Node
 {
+public:
+  DEPTH_IMAGE_PROC_PUBLIC CropForemostNode();
+
+private:
   // Subscriptions
-  boost::shared_ptr<image_transport::ImageTransport> it_;
   image_transport::Subscriber sub_raw_;
 
   // Publications
-  boost::mutex connect_mutex_;
+  std::mutex connect_mutex_;
   image_transport::Publisher pub_depth_;
-
-  virtual void onInit();
 
   void connectCb();
 
-  void depthCb(const sensor_msgs::ImageConstPtr& raw_msg);
+  void depthCb(const sensor_msgs::msg::Image::ConstSharedPtr & raw_msg);
 
   double distance_;
+
+  rclcpp::Logger logger_ = rclcpp::get_logger("CropForemostNode");
 };
 
-void CropForemostNodelet::onInit()
+CropForemostNode::CropForemostNode()
+: Node("CropForemostNode")
 {
-  ros::NodeHandle& nh = getNodeHandle();
-  ros::NodeHandle& private_nh = getPrivateNodeHandle();
-  private_nh.getParam("distance", distance_);
-  it_.reset(new image_transport::ImageTransport(nh));
+  this->get_parameter("distance", distance_);
 
   // Monitor whether anyone is subscribed to the output
-  image_transport::SubscriberStatusCallback connect_cb = boost::bind(&CropForemostNodelet::connectCb, this);
+  // TODO(ros2) Implement when SubscriberStatusCallback is available
+  // image_transport::SubscriberStatusCallback connect_cb =
+  //   std::bind(&CropForemostNode::connectCb, this);
+  connectCb();
   // Make sure we don't enter connectCb() between advertising and assigning to pub_depth_
-  boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  pub_depth_ = it_->advertise("image", 1, connect_cb, connect_cb);
+  std::lock_guard<std::mutex> lock(connect_mutex_);
+  // TODO(ros2) Implement when SubscriberStatusCallback is available
+  // pub_depth_ = it_->advertise("image", 1, connect_cb, connect_cb);
+  pub_depth_ = image_transport::create_publisher(this, "image");
 }
 
 // Handles (un)subscribing when clients (un)subscribe
-void CropForemostNodelet::connectCb()
+void CropForemostNode::connectCb()
 {
-  boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  if (pub_depth_.getNumSubscribers() == 0)
-  {
+  std::lock_guard<std::mutex> lock(connect_mutex_);
+  // TODO(ros2) Implement getNumSubscribers when rcl/rmw support it
+  // if (pub_depth_.getNumSubscribers() == 0)
+  if (0) {
     sub_raw_.shutdown();
-  }
-  else if (!sub_raw_)
-  {
-    image_transport::TransportHints hints("raw", ros::TransportHints(), getPrivateNodeHandle());
-    sub_raw_ = it_->subscribe("image_raw", 1, &CropForemostNodelet::depthCb, this, hints);
+  } else if (!sub_raw_) {
+    image_transport::TransportHints hints(this, "raw");
+    sub_raw_ = image_transport::create_subscription(this, "image_raw",
+        std::bind(&CropForemostNode::depthCb, this, std::placeholders::_1),
+        hints.getTransport());
   }
 }
 
-void CropForemostNodelet::depthCb(const sensor_msgs::ImageConstPtr& raw_msg)
+void CropForemostNode::depthCb(const sensor_msgs::msg::Image::ConstSharedPtr & raw_msg)
 {
   cv_bridge::CvImagePtr cv_ptr;
-  try
-  {
+  try {
     cv_ptr = cv_bridge::toCvCopy(raw_msg);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
+  } catch (cv_bridge::Exception & e) {
+    RCLCPP_ERROR(logger_, "cv_bridge exception: %s", e.what());
     return;
   }
 
   // Check the number of channels
-  if(sensor_msgs::image_encodings::numChannels(raw_msg->encoding) != 1){
-    NODELET_ERROR_THROTTLE(2, "Only grayscale image is acceptable, got [%s]", raw_msg->encoding.c_str());
+  if (sensor_msgs::image_encodings::numChannels(raw_msg->encoding) != 1) {
+    RCLCPP_ERROR(logger_, "Only grayscale image is acceptable, got [%s]",
+      raw_msg->encoding.c_str());
     return;
   }
 
@@ -114,7 +118,7 @@ void CropForemostNodelet::depthCb(const sensor_msgs::ImageConstPtr& raw_msg)
   cv::minMaxIdx(cv_ptr->image, &minVal, 0, 0, 0, cv_ptr->image != 0);
 
   int imtype = cv_bridge::getCvType(raw_msg->encoding);
-  switch (imtype){
+  switch (imtype) {
     case CV_8UC1:
     case CV_8SC1:
     case CV_32F:
@@ -135,8 +139,9 @@ void CropForemostNodelet::depthCb(const sensor_msgs::ImageConstPtr& raw_msg)
   pub_depth_.publish(cv_ptr->toImageMsg());
 }
 
-} // namespace depth_image_proc
+}  // namespace depth_image_proc
 
-// Register as nodelet
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(depth_image_proc::CropForemostNodelet,nodelet::Nodelet);
+#include "class_loader/register_macro.hpp"
+
+// Register the component with class_loader.
+CLASS_LOADER_REGISTER_CLASS(depth_image_proc::CropForemostNode, rclcpp::Node)
