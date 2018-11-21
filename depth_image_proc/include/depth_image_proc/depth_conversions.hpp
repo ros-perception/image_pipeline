843b932985acc41889071b1749a1e13d80ea58c2
Chris Ye <chris.ye@intel.com>
2018-11-22 06:03:09 +0800

    Port depth image proc on ROS2 (#362)
    * Port depth_image_proc on ROS2
    
    * Port depth_image_proc of image_pipeline on ROS2
    
    * rename Nodelets as Node
    
    * add launch examples, such as "ros2 launch depth_image_proc point_cloud_xyzi.launch.py"
    
    * verified point_cloud_xyzrgb, point_cloud_xyz, convert_metric based on Realsense camera(https://github.com/intel/ros2_intel_realsense).
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * add ament_lint_auto test and adjust code style
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * update test example for depth_image_proc
    
    - rename raw topic as image_transport fixed the issue (https://github.com/ros-perception/image_common/issues/96)
    - update test example of point_cloud_xyzrgb.launch
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * remove unused dependence in cmakelist
    
    * remove boost which is unused on ROS2
    
    * remove cv_bridge version check logic as setted in find_package
    
    * update maintainer in package.xml
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * added all example launchers for demo test
    
    pass test:
     ros2 launch depth_image_proc point_cloud_xyzrgb.launch.py
     ros2 launch depth_image_proc point_cloud_xyz.launch.py
     ros2 launch depth_image_proc convert_metric.launch.py
     ros2 launch depth_image_proc crop_foremost.launch.py
     ros2 launch depth_image_proc point_cloud_xyz_radial.launch.py
     ros2 launch depth_image_proc disparity.launch.py
     ros2 launch depth_image_proc register.launch.py
     ros2 launch depth_image_proc point_cloud_xyzi.launch.py
     ros2 launch depth_image_proc point_cloud_xyzi_radial.launch.py
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>
    
    * @wip update to use raw pointers.
    
    * continue to update to use raw pointer
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>

---
+++ depth_image_proc/include/depth_image_proc/depth_conversions.hpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2008, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the Willow Garage nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#ifndef DEPTH_IMAGE_PROC__DEPTH_CONVERSIONS_HPP_
+#define DEPTH_IMAGE_PROC__DEPTH_CONVERSIONS_HPP_
+
+#include <sensor_msgs/msg/image.h>
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <image_geometry/pinhole_camera_model.h>
+#include <depth_image_proc/depth_traits.hpp>
+
+#include <limits>
+
+namespace depth_image_proc
+{
+
+using PointCloud = sensor_msgs::msg::PointCloud2;
+
+// Handles float or uint16 depths
+template<typename T>
+void convert(
+  const sensor_msgs::msg::Image::ConstSharedPtr & depth_msg,
+  PointCloud::SharedPtr & cloud_msg,
+  const image_geometry::PinholeCameraModel & model,
+  double range_max = 0.0)
+{
+  // Use correct principal point from calibration
+  float center_x = model.cx();
+  float center_y = model.cy();
+
+  // Combine unit conversion (if necessary) with scaling by focal length for computing (X,Y)
+  double unit_scaling = DepthTraits<T>::toMeters(T(1) );
+  float constant_x = unit_scaling / model.fx();
+  float constant_y = unit_scaling / model.fy();
+  float bad_point = std::numeric_limits<float>::quiet_NaN();
+
+  sensor_msgs::PointCloud2Iterator<float> iter_x(*cloud_msg, "x");
+  sensor_msgs::PointCloud2Iterator<float> iter_y(*cloud_msg, "y");
+  sensor_msgs::PointCloud2Iterator<float> iter_z(*cloud_msg, "z");
+  const T * depth_row = reinterpret_cast<const T *>(&depth_msg->data[0]);
+  int row_step = depth_msg->step / sizeof(T);
+  for (int v = 0; v < static_cast<int>(cloud_msg->height); ++v, depth_row += row_step) {
+    for (int u = 0; u < static_cast<int>(cloud_msg->width); ++u, ++iter_x, ++iter_y, ++iter_z) {
+      T depth = depth_row[u];
+
+      // Missing points denoted by NaNs
+      if (!DepthTraits<T>::valid(depth)) {
+        if (range_max != 0.0) {
+          depth = DepthTraits<T>::fromMeters(range_max);
+        } else {
+          *iter_x = *iter_y = *iter_z = bad_point;
+          continue;
+        }
+      }
+
+      // Fill in XYZ
+      *iter_x = (u - center_x) * depth * constant_x;
+      *iter_y = (v - center_y) * depth * constant_y;
+      *iter_z = DepthTraits<T>::toMeters(depth);
+    }
+  }
+}
+
+}  // namespace depth_image_proc
+
+#endif  // DEPTH_IMAGE_PROC__DEPTH_CONVERSIONS_HPP_
--- depth_image_proc/src/nodelets/point_cloud_xyz.cpp
+++ depth_image_proc/src/nodelets/point_cloud_xyz.cpp
@@ -1,110 +1,125 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
-#include <ros/ros.h>
-#include <nodelet/nodelet.h>
+// Copyright (c) 2008, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the Willow Garage nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <rclcpp/rclcpp.hpp>
 #include <image_transport/image_transport.h>
-#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/image_encodings.hpp>
 #include <image_geometry/pinhole_camera_model.h>
-#include <boost/thread.hpp>
-#include <depth_image_proc/depth_conversions.h>
+#include <depth_image_proc/depth_conversions.hpp>
+#include <depth_image_proc/visibility.h>
 
-#include <sensor_msgs/point_cloud2_iterator.h>
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <memory>
 
-namespace depth_image_proc {
+namespace depth_image_proc
+{
 
 namespace enc = sensor_msgs::image_encodings;
 
-class PointCloudXyzNodelet : public nodelet::Nodelet
+class PointCloudXyzNode : public rclcpp::Node
 {
+public:
+  DEPTH_IMAGE_PROC_PUBLIC PointCloudXyzNode();
+
+private:
+  using PointCloud2 = sensor_msgs::msg::PointCloud2;
+  using Image = sensor_msgs::msg::Image;
+  using CameraInfo = sensor_msgs::msg::CameraInfo;
+
   // Subscriptions
-  boost::shared_ptr<image_transport::ImageTransport> it_;
   image_transport::CameraSubscriber sub_depth_;
   int queue_size_;
 
   // Publications
-  boost::mutex connect_mutex_;
-  typedef sensor_msgs::PointCloud2 PointCloud;
-  ros::Publisher pub_point_cloud_;
+  std::mutex connect_mutex_;
+  rclcpp::Publisher<PointCloud2>::SharedPtr pub_point_cloud_;
 
   image_geometry::PinholeCameraModel model_;
 
-  virtual void onInit();
-
   void connectCb();
 
-  void depthCb(const sensor_msgs::ImageConstPtr& depth_msg,
-               const sensor_msgs::CameraInfoConstPtr& info_msg);
+  void depthCb(
+    const Image::ConstSharedPtr & depth_msg,
+    const CameraInfo::ConstSharedPtr & info_msg);
+
+  rclcpp::Logger logger_ = rclcpp::get_logger("PointCloudXyzNode");
 };
 
-void PointCloudXyzNodelet::onInit()
+PointCloudXyzNode::PointCloudXyzNode()
+: Node("PointCloudXyzNode")
 {
-  ros::NodeHandle& nh         = getNodeHandle();
-  ros::NodeHandle& private_nh = getPrivateNodeHandle();
-  it_.reset(new image_transport::ImageTransport(nh));
-
   // Read parameters
-  private_nh.param("queue_size", queue_size_, 5);
+  this->get_parameter_or("queue_size", queue_size_, 5);
 
   // Monitor whether anyone is subscribed to the output
-  ros::SubscriberStatusCallback connect_cb = boost::bind(&PointCloudXyzNodelet::connectCb, this);
+  // TODO(ros2) Implement when SubscriberStatusCallback is available
+  // ros::SubscriberStatusCallback connect_cb = boost::bind(&PointCloudXyzNode::connectCb, this);
+  connectCb();
+
   // Make sure we don't enter connectCb() between advertising and assigning to pub_point_cloud_
-  boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  pub_point_cloud_ = nh.advertise<PointCloud>("points", 1, connect_cb, connect_cb);
+  std::lock_guard<std::mutex> lock(connect_mutex_);
+  // TODO(ros2) Implement when SubscriberStatusCallback is available
+  // pub_point_cloud_ = nh.advertise<PointCloud>("points", 1, connect_cb, connect_cb);
+  pub_point_cloud_ = create_publisher<PointCloud2>("points");
 }
 
 // Handles (un)subscribing when clients (un)subscribe
-void PointCloudXyzNodelet::connectCb()
+void PointCloudXyzNode::connectCb()
 {
-  boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  if (pub_point_cloud_.getNumSubscribers() == 0)
-  {
+  std::lock_guard<std::mutex> lock(connect_mutex_);
+  // TODO(ros2) Implement getNumSubscribers when rcl/rmw support it
+  // if (pub_point_cloud_->getNumSubscribers() == 0)
+  if (0) {
     sub_depth_.shutdown();
-  }
-  else if (!sub_depth_)
-  {
-    image_transport::TransportHints hints("raw", ros::TransportHints(), getPrivateNodeHandle());
-    sub_depth_ = it_->subscribeCamera("image_rect", queue_size_, &PointCloudXyzNodelet::depthCb, this, hints);
+  } else if (!sub_depth_) {
+    auto custom_qos = rmw_qos_profile_system_default;
+    custom_qos.depth = queue_size_;
+
+    sub_depth_ = image_transport::create_camera_subscription(
+      this,
+      "image_rect",
+      std::bind(&PointCloudXyzNode::depthCb, this, std::placeholders::_1, std::placeholders::_2),
+      "raw",
+      custom_qos);
   }
 }
 
-void PointCloudXyzNodelet::depthCb(const sensor_msgs::ImageConstPtr& depth_msg,
-                                   const sensor_msgs::CameraInfoConstPtr& info_msg)
+void PointCloudXyzNode::depthCb(
+  const Image::ConstSharedPtr & depth_msg,
+  const CameraInfo::ConstSharedPtr & info_msg)
 {
-  PointCloud::Ptr cloud_msg(new PointCloud);
+  auto cloud_msg = std::make_shared<PointCloud2>();
   cloud_msg->header = depth_msg->header;
   cloud_msg->height = depth_msg->height;
-  cloud_msg->width  = depth_msg->width;
+  cloud_msg->width = depth_msg->width;
   cloud_msg->is_dense = false;
   cloud_msg->is_bigendian = false;
 
@@ -114,25 +129,21 @@ void PointCloudXyzNodelet::depthCb(const sensor_msgs::ImageConstPtr& depth_msg,
   // Update camera model
   model_.fromCameraInfo(info_msg);
 
-  if (depth_msg->encoding == enc::TYPE_16UC1)
-  {
+  if (depth_msg->encoding == enc::TYPE_16UC1) {
     convert<uint16_t>(depth_msg, cloud_msg, model_);
-  }
-  else if (depth_msg->encoding == enc::TYPE_32FC1)
-  {
+  } else if (depth_msg->encoding == enc::TYPE_32FC1) {
     convert<float>(depth_msg, cloud_msg, model_);
-  }
-  else
-  {
-    NODELET_ERROR_THROTTLE(5, "Depth image has unsupported encoding [%s]", depth_msg->encoding.c_str());
+  } else {
+    RCLCPP_ERROR(logger_, "Depth image has unsupported encoding [%s]", depth_msg->encoding.c_str());
     return;
   }
 
-  pub_point_cloud_.publish (cloud_msg);
+  pub_point_cloud_->publish(cloud_msg);
 }
 
-} // namespace depth_image_proc
+}  // namespace depth_image_proc
 
-// Register as nodelet
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(depth_image_proc::PointCloudXyzNodelet,nodelet::Nodelet);
+#include "class_loader/register_macro.hpp"
+
+// Register the component with class_loader.
+CLASS_LOADER_REGISTER_CLASS(depth_image_proc::PointCloudXyzNode, rclcpp::Node)
