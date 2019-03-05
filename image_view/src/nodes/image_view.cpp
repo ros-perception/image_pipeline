8d9f04de53da87df2a5af71edf1d0aa9d869adf1
rnd <robin.dietrich@mojin-robotics.de>
2019-03-05 15:52:38 +0100

    Added a param to specify whether or not to show image in fullscreen view.

--- image_view/src/nodes/image_view.cpp
+++ image_view/src/nodes/image_view.cpp
@@ -162,6 +162,13 @@ int main(int argc, char **argv)
     cv::namedWindow(g_window_name, autosize ? (CV_WINDOW_AUTOSIZE | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED) : 0);
     cv::setMouseCallback(g_window_name, &mouseCb);
 
+    // Define whether or not the image should be displayed in fullscreen mode
+    bool fullscreen;
+    local_nh.param("fullscreen", fullscreen, false);
+    if (fullscreen) {
+        cv::setWindowProperty(g_window_name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+    }
+
     if(autosize == false)
     {
       if(local_nh.hasParam("width") && local_nh.hasParam("height"))
