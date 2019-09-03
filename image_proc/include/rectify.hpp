ff7f6980b13099e87b8bc147c36fb6af2a8a6302
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-09-03 12:04:02 -0500

    Revert "ROS2 Image proc refactoring using components and added launch file"

--- image_proc/include/rectify.hpp
+++ image_proc/include/rectify.hpp
@@ -57,9 +57,7 @@ class RectifyNode : public rclcpp::Node
     int interpolation;
     std::string camera_namespace_;
     std::string image_rect;
-    std::string ns_image_topic_;
-
-    std::string image_topic_;
+    std::string image_topic;
 
     std::mutex connect_mutex_;
     image_transport::Publisher pub_rect_;
@@ -67,7 +65,6 @@ class RectifyNode : public rclcpp::Node
     // Processing state (note: only safe because we're using single-threaded NodeHandle!)
     image_geometry::PinholeCameraModel model_;
 
-    void createPublisher(const std::string & parameter_name);
     void connectCb();
     void imageCb(const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
                 const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info_msg);
