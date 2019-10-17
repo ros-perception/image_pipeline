727a924688e61a5be65142117bdc908720e2beeb
Chris Ye <chris.ye@intel.com>
2019-10-17 23:47:04 +0800

    add launch examples for image and mono and stereo (#386)
    * [image_publisher] launch examples for file and mono and stereo
    
    launch examples to support:
    1) load local image file and publish to the ros topic
    1) load mono usb camera /dev/video0 and publish to the ros topic
    1) load two usb cameras /dev/video0 amd /dev/video1, and publish to the left and right ros topic, this could simulate stereo camera.
    
    * Publish image file to topic
      $ros2 launch image_publisher image_publisher_file.launch.py
        /camera/camera_info
        /camera/image_raw
    
    * Publish monocular camera (/dev/video0)
      $ros2 launch image_publisher image_publisher_mono.launch.py
        /camera/camera_info
        /camera/image_raw
    
    * Publish stereo camera (/dev/video0 and /dev/video1)
      $ros2 launch image_publisher image_publisher_stereo.launch.py
        /left/camera_info
        /left/image_raw
        /right/camera_info
        /right/image_raw
    
    * [image_publisher] fix for lint check issue
    
    fix lint check error in launch file

---
+++ image_publisher/examples/launch/image_publisher_file.launch.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+import launch_ros.actions
+
+
+def generate_launch_description():
+    filename = os.path.join(get_package_share_directory('image_publisher'), 'launch',
+                            'splash.png')
+    return LaunchDescription([
+
+        launch_ros.actions.Node(
+            package='image_publisher', node_executable='image_publisher', output='screen',
+            arguments=[filename],
+            remappings=[('image_raw', '/camera/image_raw'),
+                        ('camera_info', '/camera/camera_info')]),
+    ])
