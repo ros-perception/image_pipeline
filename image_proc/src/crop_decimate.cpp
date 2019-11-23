87f83c3e7063dabbfee52bf811086aef3fc41293
Andreas Klintberg <ankl@kth.se>
2019-11-23 15:57:33 -0800

    preprocessor condition for different opencv version

--- image_proc/src/crop_decimate.cpp
+++ image_proc/src/crop_decimate.cpp
@@ -46,7 +46,11 @@ void debayer2x2toBGR(
   int R, int G1, int G2, int B)
 {
   typedef cv::Vec<T, 3> DstPixel;  // 8- or 16-bit BGR
-  dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
+  #if CV_MAJOR_VERSION >= 3 && CV_MINOR_VERSION > 2
+      dst.create(src.rows / 2, src.cols / 2, cv::traits::Type<DstPixel>::value);
+  #else
+      dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
+  #endif
 
   int src_row_step = src.step1();
   int dst_row_step = dst.step1();
