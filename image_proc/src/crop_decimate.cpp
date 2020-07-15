0f895597ddb688213d1fe5dcfc71ee8fe2dc6c1c
Chris Lalancette <clalancette@openrobotics.org>
2020-07-15 21:12:41 +0000

    Fixes from review.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- image_proc/src/crop_decimate.cpp
+++ image_proc/src/crop_decimate.cpp
@@ -46,12 +46,11 @@ void debayer2x2toBGR(
   int R, int G1, int G2, int B)
 {
   typedef cv::Vec<T, 3> DstPixel;  // 8- or 16-bit BGR
-#if CV_VERSION_MAJOR >= 3 && CV_VERSION_MINOR >= 2
-  // Use OpenCV 3 API
-  dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
-#else
-  // Assume OpenCV 4
+#if CV_VERSION_MAJOR >= 4
   dst.create(src.rows / 2, src.cols / 2, cv::traits::DataType<DstPixel>::type);
+#else
+  // Assume OpenCV 3 API
+  dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
 #endif
 
   int src_row_step = src.step1();
