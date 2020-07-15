2e0c6d42cb650534e4aeea586482030e5c0d46c8
Chris Lalancette <clalancette@gmail.com>
2020-07-15 17:34:36 -0400

    Opencv 3 compatibility (#564)
    * Remove GTK from image_view.
    
    It is no longer used at all in image_view.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Reinstate OpenCV 3 compatibility.
    
    While Foxy only supports Ubuntu 20.04 (and hence OpenCV 4),
    we still strive to maintain Ubuntu 18.04 (which has OpenCV 3).
    In this case, it is trivial to keep keep image_pipeline working
    with OpenCV 3, so reintroduce compatibility with it.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Fixes from review.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * One more fix.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- image_proc/src/crop_decimate.cpp
+++ image_proc/src/crop_decimate.cpp
@@ -46,7 +46,12 @@ void debayer2x2toBGR(
   int R, int G1, int G2, int B)
 {
   typedef cv::Vec<T, 3> DstPixel;  // 8- or 16-bit BGR
+#if CV_VERSION_MAJOR >= 4
   dst.create(src.rows / 2, src.cols / 2, cv::traits::Type<DstPixel>::value);
+#else
+  // Assume OpenCV 3 API
+  dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
+#endif
 
   int src_row_step = src.step1();
   int dst_row_step = dst.step1();
