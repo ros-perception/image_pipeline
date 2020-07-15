9873b9fa7664260da10478d2253e95bcf81dcd04
Chris Lalancette <clalancette@openrobotics.org>
2020-07-15 20:30:12 +0000

    Reinstate OpenCV 3 compatibility.
    While Foxy only supports Ubuntu 20.04 (and hence OpenCV 4),
    we still strive to maintain Ubuntu 18.04 (which has OpenCV 3).
    In this case, it is trivial to keep keep image_pipeline working
    with OpenCV 3, so reintroduce compatibility with it.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- image_proc/src/crop_decimate.cpp
+++ image_proc/src/crop_decimate.cpp
@@ -46,7 +46,13 @@ void debayer2x2toBGR(
   int R, int G1, int G2, int B)
 {
   typedef cv::Vec<T, 3> DstPixel;  // 8- or 16-bit BGR
-  dst.create(src.rows / 2, src.cols / 2, cv::traits::Type<DstPixel>::value);
+#if CV_VERSION_MAJOR >= 3 && CV_VERSION_MINOR >= 2
+  // Use OpenCV 3 API
+  dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
+#else
+  // Assume OpenCV 4
+  dst.create(src.rows / 2, src.cols / 2, cv::traits::DataType<DstPixel>::type);
+#endif
 
   int src_row_step = src.step1();
   int dst_row_step = dst.step1();
