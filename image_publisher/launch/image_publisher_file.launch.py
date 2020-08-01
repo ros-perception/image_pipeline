b16b5d8567634d66ff7ae57e274edc4436f29f9d
louis <louis.tran@otsaw.com>
2020-08-02 02:37:53 +0800

    [Foxy][Image Publisher] Update launch file

--- image_publisher/launch/image_publisher_file.launch.py
+++ image_publisher/launch/image_publisher_file.launch.py
@@ -43,7 +43,7 @@ def generate_launch_description():
     return LaunchDescription([
 
         launch_ros.actions.Node(
-            package='image_publisher', node_executable='image_publisher', output='screen',
+            package='image_publisher', executable='image_publisher_node', output='screen',
             arguments=[filename],
             remappings=[('image_raw', '/camera/image_raw'),
                         ('camera_info', '/camera/camera_info')]),
