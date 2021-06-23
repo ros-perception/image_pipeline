e692b0cb99f1d9f302c9cbc03db090530acd4b66
Patrick Musau <pmusau13ster@gmail.com>
2021-06-23 15:08:06 -0500

    changes per comments
    Signed-off-by: Patrick Musau <pmusau13ster@gmail.com>

--- stereo_image_proc/test/test_disparity_node.py
+++ stereo_image_proc/test/test_disparity_node.py
@@ -74,7 +74,6 @@ def generate_test_description():
             executable='disparity_node',
             name='disparity_node',
             parameters=[
-                {"approximate_sync": True},
                 {"use_system_default_qos": True}
             ],
             output='screen'
