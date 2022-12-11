b3af566ed7b349fb29b1da0661ff1d6f57c5ebcd
Christian Rauch <Rauch.Christian@gmx.de>
2022-12-11 19:19:56 +0100

    use remap rules instead of parameters for services

--- camera_calibration/src/camera_calibration/camera_calibrator.py
+++ camera_calibration/src/camera_calibration/camera_calibrator.py
@@ -97,13 +97,9 @@ class CalibrationNode(Node):
                  max_chessboard_speed = -1, queue_size = 1):
         super().__init__(name)
 
-        left_camera = self.declare_parameter("left_camera", "left_camera").get_parameter_value().string_value
-        right_camera = self.declare_parameter("right_camera", "right_camera").get_parameter_value().string_value
-        camera = self.declare_parameter("camera", "camera").get_parameter_value().string_value
-
-        self.set_camera_info_service = self.create_client(sensor_msgs.srv.SetCameraInfo, camera + "/set_camera_info")
-        self.set_left_camera_info_service = self.create_client(sensor_msgs.srv.SetCameraInfo, left_camera + "/set_camera_info")
-        self.set_right_camera_info_service = self.create_client(sensor_msgs.srv.SetCameraInfo, right_camera + "/set_camera_info")
+        self.set_camera_info_service = self.create_client(sensor_msgs.srv.SetCameraInfo, "camera/set_camera_info")
+        self.set_left_camera_info_service = self.create_client(sensor_msgs.srv.SetCameraInfo, "left_camera/set_camera_info")
+        self.set_right_camera_info_service = self.create_client(sensor_msgs.srv.SetCameraInfo, "right_camera/set_camera_info")
 
         if service_check:
             available = False
