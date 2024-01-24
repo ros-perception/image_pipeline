551bc00aa9deab54b2c2ec36bd73d145219bea18
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-01-24 16:47:35 +0100

    [backport humble] Fixed image types in depth_image_proc (#916)
    backport humble
    https://github.com/ros-perception/image_pipeline/pull/915#event-11585393591
    
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- depth_image_proc/src/point_cloud_xyzrgb.cpp
+++ depth_image_proc/src/point_cloud_xyzrgb.cpp
@@ -267,6 +267,10 @@ void PointCloudXyzrgbNode::imageCb(
     convertRgb(rgb_msg, cloud_msg, red_offset, green_offset, blue_offset, color_step);
   } else if (rgb_msg->encoding == sensor_msgs::image_encodings::BGR8) {
     convertRgb(rgb_msg, cloud_msg, red_offset, green_offset, blue_offset, color_step);
+  } else if (rgb_msg->encoding == sensor_msgs::image_encodings::BGRA8) {
+    convertRgb(rgb_msg, cloud_msg, red_offset, green_offset, blue_offset, color_step);
+  } else if (rgb_msg->encoding == sensor_msgs::image_encodings::RGBA8) {
+    convertRgb(rgb_msg, cloud_msg, red_offset, green_offset, blue_offset, color_step);
   } else if (rgb_msg->encoding == sensor_msgs::image_encodings::MONO8) {
     convertRgb(rgb_msg, cloud_msg, red_offset, green_offset, blue_offset, color_step);
   } else {
