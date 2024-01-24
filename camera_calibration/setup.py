0da04cb9dbacf7eb11456101c5de2eaf2b5e187f
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-01-24 11:21:41 +0100

    3.0.3

--- camera_calibration/setup.py
+++ camera_calibration/setup.py
@@ -5,7 +5,7 @@ PACKAGE_NAME = "camera_calibration"
 
 setup(
     name=PACKAGE_NAME,
-    version='3.0.2',
+    version='3.0.3',
     packages=["camera_calibration", "camera_calibration.nodes"],
     data_files=[
     ('share/ament_index/resource_index/packages',
