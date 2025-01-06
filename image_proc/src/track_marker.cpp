90395ec12c8b8b6f0b16b86f7808c91632594483
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-01-06 18:47:05 +0100

    make linters happy
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- image_proc/src/track_marker.cpp
+++ image_proc/src/track_marker.cpp
@@ -30,8 +30,6 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include <tf2/LinearMath/Quaternion.hpp>
-
 #include <cstddef>
 #include <functional>
 #include <memory>
@@ -45,6 +43,7 @@
 #include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/LinearMath/Quaternion.hpp>
 
 namespace image_proc
 {
