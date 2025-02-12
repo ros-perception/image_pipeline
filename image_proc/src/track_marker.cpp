bc5d7ed60b02dd62d6549b759b32ce4f753e4980
Bernd Müller <github@muellerbernd.de>
2025-02-12 10:42:53 +0100

    [fix] based on https://github.com/ros/meta-ros/commit/719fe6e421af07a16e4958036beadd50ae0ef5fb

--- image_proc/src/track_marker.cpp
+++ image_proc/src/track_marker.cpp
@@ -64,9 +64,17 @@ TrackMarkerNode::TrackMarkerNode(const rclcpp::NodeOptions & options)
   marker_size_ = this->declare_parameter("marker_size", 0.05);
   // Default dictionary is cv::aruco::DICT_6X6_250
   int dict_id = this->declare_parameter("dictionary", 10);
-
+  #if CV_VERSION_MAJOR > 4 || CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 7
+  detector_params_ = cv::makePtr<cv::aruco::DetectorParameters>();
+  #else
   detector_params_ = cv::aruco::DetectorParameters::create();
+  #endif
+
+  #if CV_VERSION_MAJOR > 4 || CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 7
+  dictionary_ = cv::makePtr<cv::aruco::Dictionary>(cv::aruco::getPredefinedDictionary(dict_id));
+  #else
   dictionary_ = cv::aruco::getPredefinedDictionary(dict_id);
+  #endif
 
   // Setup lazy subscriber using publisher connection callback
   rclcpp::PublisherOptions pub_options;
