d309aeae5b6bc03923e465e06d551a64a29b79d5
Christian Rauch <Christian.Rauch@unileoben.ac.at>
2025-02-18 13:34:01 +0100

    fix RealSense package and node name

--- depth_image_proc/launch/disparity.launch.py
+++ depth_image_proc/launch/disparity.launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
     return LaunchDescription([
         # install realsense from https://github.com/intel/ros2_intel_realsense
         launch_ros.actions.Node(
-            package='realsense_ros2_camera', executable='realsense_ros2_camera',
+            package='realsense2_camera', executable='realsense2_camera_node',
             output='screen'),
 
         # we use realsense camera for test, realsense not support left and right topic
