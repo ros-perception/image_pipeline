ca650ddad9960a8858a5f12c13957871fa8b72dc
Christian Rauch <Christian.Rauch@unileoben.ac.at>
2025-02-18 18:05:04 +0100

    fix depth_image_proc launch files (#1077)
    A couple of fixes to make the `depth_image_proc` launch files work
    again. The issues are mostly caused by API with launch and the RealSense
    node.
    
    (cherry picked from commit d541f3061696abba489d1421ff723b4730f72463)

--- depth_image_proc/launch/point_cloud_xyzrgb.launch.py
+++ depth_image_proc/launch/point_cloud_xyzrgb.launch.py
@@ -45,7 +45,10 @@ def generate_launch_description():
     return LaunchDescription([
         # install realsense from https://github.com/intel/ros2_intel_realsense
         launch_ros.actions.Node(
-            package='realsense_ros2_camera', node_executable='realsense_ros2_camera',
+            package='realsense2_camera',
+            executable='realsense2_camera_node',
+            namespace='',
+            parameters=[{'align_depth.enable': True},],
             output='screen'),
 
         # launch plugin through rclcpp_components container
@@ -71,6 +74,6 @@ def generate_launch_description():
 
         # rviz
         launch_ros.actions.Node(
-            package='rviz2', node_executable='rviz2', output='screen',
+            package='rviz2', executable='rviz2', output='screen',
             arguments=['--display-config', default_rviz]),
     ])
